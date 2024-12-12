afe54b297ee4d7780e4637b6442362981daa361d
Bohdan Pylypchenko <Bohdan314159yLypchenko@proton.me>
2024-12-12 19:07:56 +0200

    - 77788 - connections - can I use bitset for StateVector???

--- sr-research/main.cpp
+++ sr-research/main.cpp
@@ -9,24 +9,14 @@ int main()
     //print("\n=== simple ===\n");
     //sr::research::simple();
 
-    //print("\n=== original s23 ===\n");
-    //sr::research::s23_original();
+    print("\n=== original s23 ===\n");
+    sr::research::s23_original();
 
     print("\n=== s23 with rt (7 7 7 8 8) ===\n");
     sr::research::s23_rt_7_7_7_8_8();
 
-    //print("\n=== s23 with medium rt (greedy) ===\n");
-    //sr::research::s23_rt_medium_greedy();
+    print("\n=== s23 with rt 1: modified connections");
+    sr::research::s23_rt_7_7_7_8_8_1_modified_connections();
 
-    //print("\n=== s23 with large rt (brute force) ===\n");
-    //sr::research::s23_rt_large_brute_force();
-
-    //print("\n=== s23 with large rt (greedy) ===\n");
-    //sr::research::s23_rt_large_greedy();
-
-    //print("\n=== s23 with medium rt, pr5 joined to b5 ===\n");
-    //sr::research::s23_rt_medium_join_pr5_to_b5();
-
-    //print("\n=== s23 with medium rt, pr5 joined to b5, c4 joined to b1 ===\n");
-    //sr::research::s23_rt_medium_join_pr5_to_b5_join_c4_to_b1();
+    return 0;
 }
